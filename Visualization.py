9a7e9740f661a9aa1fad808c10788aedd8a3008b
Kian Jones <kianjones9@gmail.com>
2020-01-11 23:31:34 -0500

    Basic Clock function

--- Visualization.py
+++ Visualization.py
@@ -8,11 +8,10 @@ with open("Murder-on-the-2nd-Floor-Raw-Data.json") as file:
 pygame.init()
 pygame.font.init()
 
-window_width = 745
+window_width = 1200
 window_height = 744
 
-animation_increment = 10
-clock_tick_rate = 20
+clock_tick_rate = 5
 
 size = (window_width, window_height)
 screen = pygame.display.set_mode(size)
@@ -25,10 +24,12 @@ clock = pygame.time.Clock()
 background_image = pygame.image.load("Floor-Plan.png").convert()
 
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-
+screen.fill(pygame.Color("white"))
 screen.blit(background_image, [0, 0])
 
 paused = False
+
+current_time = 1578151801
 
 while not complete:
 
@@ -46,11 +47,14 @@ while not complete:
         #do nothing
         pass
     else:
-        for event in data:
-            time = my_font.render("Time: " + event, False, (0, 0, 0))
-            screen.blit(time, (0,0))
 
+        screen.fill(pygame.Color("white"), (900, 0, 300, 100))
+        time = my_font.render("Time: " + str(current_time), False, (0, 0, 0))
+        screen.blit(time, (900,0))
 
+    if not paused:
+        current_time += 1
+        pygame.display.flip()
+        clock.tick(clock_tick_rate)
 
-    pygame.display.flip()
-    clock.tick(clock_tick_rate)
+pygame.quit()
